a00085a50330c2a78ba11efafaec3452fe254b66
Habeeb <habeebahmadu1@gmail.com>
2025-04-16 23:17:08 +0100

    feat: setup tarnstack react query to handle data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { router } from "./routes";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>
-      <h1>HOME PAGE</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/api/fetch-articles.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_NYT_API_KEY;
+
+const URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=${API_KEY}`;
+
+export const fetchPopularArticles = async (period = 7) => {
+  const { data } = await axios.get(URL);
+  return data;
+};
