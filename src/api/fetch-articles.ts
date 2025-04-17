4d01ed39680ffa5a30ab17986292d283f06c6698
Habeeb <habeebahmadu1@gmail.com>
2025-04-17 23:31:31 +0100

    fix: fetch articles bug

--- src/api/fetch-articles.ts
+++ src/api/fetch-articles.ts
@@ -4,7 +4,7 @@ const API_KEY = import.meta.env.VITE_NYT_API_KEY;
 
 const URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=${API_KEY}`;
 
-export const fetchPopularArticles = async (period = 7) => {
+export const fetchPopularArticles = async () => {
   const { data } = await axios.get(URL);
   return data;
 };
